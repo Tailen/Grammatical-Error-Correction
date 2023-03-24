4d7e25ab5d14c152f46916aafbd9ed0223758fdd
Hao Mei <haomei@andrew.cmu.edu>
2023-03-24 16:24:20 +0300

    add training metric evaluation

--- train.py
+++ train.py
@@ -10,11 +10,15 @@ import wandb
 import argparse
 from torch.utils.data import Dataset
 from transformers import T5Tokenizer, T5ForConditionalGeneration, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer
+from datasets import load_metric
 
 
 # Check GPU availability
 assert(torch.cuda.is_available())
 device = "cuda"
+
+# Load validation metric
+gleu_metric = load_metric("google_bleu")
 
 
 class C4200MDataset(Dataset):
@@ -44,12 +48,12 @@ class C4200MDataset(Dataset):
         }
 
 
-def main(train_file, val_file):
+def main(train_file, val_file, output_dir):
     # Init and config wandb
     wandb.init(project="GEC")
     config = wandb.config           # Initialize config
-    config.TRAIN_BATCH_SIZE = 48    # batch size for training
-    config.VALID_BATCH_SIZE = 256   # batch size for testing
+    config.TRAIN_BATCH_SIZE = 64    # batch size for training
+    config.VALID_BATCH_SIZE = 64    # batch size for validation
     config.TRAIN_EPOCHS = 1         # number of epochs to train
     config.LEARNING_RATE = 1e-3     # learning rate (https://discuss.huggingface.co/t/t5-finetuning-tips/684/3)
     config.SEED = 11411             # random seed (default: 42)
@@ -71,23 +75,36 @@ def main(train_file, val_file):
     train_dataset = C4200MDataset(train_df, tokenizer, config.MAX_LEN)
     val_dataset = C4200MDataset(val_df, tokenizer, config.MAX_LEN)
 
+    # Define metric function
+    def compute_metrics(evalPred):
+        preds, labels = evalPred
+        preds_decoded = tokenizer.batch_decode(preds, skip_special_tokens=True)
+        labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+        labels_decoded = tokenizer.batch_decode(labels, skip_special_tokens=True)
+        # Convert to list of list of strings for gleu metric
+        labels_decoded = [[label] for label in labels_decoded]
+        result = gleu_metric.compute(predictions=preds_decoded, references=labels_decoded)
+        return {k: round(v, 4) for k, v in result.items()}
+
     # Set training arguments
     training_args = Seq2SeqTrainingArguments(
-        output_dir="./weights",                                 # output directory
+        output_dir=output_dir,                                  # output directory
         evaluation_strategy="steps",                            # evaluation strategy to adopt during training
-        prediction_loss_only=True,                              # only return the loss when evaluating
         per_device_train_batch_size=config.TRAIN_BATCH_SIZE,    # batch size per device during training
         per_device_eval_batch_size=config.VALID_BATCH_SIZE,     # batch size for evaluation
+        gradient_accumulation_steps=1,                          # number of minibatches per gradient update
         learning_rate=config.LEARNING_RATE,                     # learning rate
         num_train_epochs=1,                                     # total number of training epochs
-        eval_steps=6000,                                        # number of update steps between two evaluations
-        save_steps=3000,                                        # after # steps model is saved
+        eval_steps=10000,                                       # number of update steps between two evaluations
+        save_steps=10000,                                       # after # steps model is saved
         save_total_limit=5,                                     # limit the total amount of checkpoints
         optim="adafactor",                                      # use adafactor optimizer to save VRAM
         tf32=True,                                              # enable 19-bit magic datatype
         bf16=True,                                              # enable mixed precision training
         load_best_model_at_end=True,                            # load best model at end of training
         # predict_with_generate=True,                             # use generate to calculate generative metrics (ROUGE, BLEU)
+        # generation_max_length=128,                              # max length of generated sequences
+        # generation_num_beams=4,                                 # number of beams for beam search
         report_to="wandb"                                       # report metrics to wandb
     )
 
@@ -99,6 +116,7 @@ def main(train_file, val_file):
         train_dataset=train_dataset,
         eval_dataset=val_dataset,
         tokenizer=tokenizer,
+        # compute_metrics=compute_metrics
     )
 
     # Train model
@@ -109,5 +127,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("train_file", type=str, default="train.csv")
     parser.add_argument("val_file", type=str, default="val.csv")
+    parser.add_argument("output_dir", type=str, default="./weights")
     args = parser.parse_args()
-    main(args.train_file, args.val_file)
+    main(args.train_file, args.val_file, args.output_dir)
